6f271e74f0617b5fed77a34d53933d5626cfeece
Lian Nivin <liannivins@gmail.com>
2021-09-20 00:05:46 -0500

    Avance - Agregar logica shuffle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'aleatory-groups';
+export class AppComponent implements OnInit{
+  title: string = 'aleatory-groups';
+  name: string = '';
+  show: boolean = false;
+  people: string[] = [];
+  numberOfGroups: number = 0;
+  shuffledPeople: string[] = [];
+
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+
+  saveName() {
+    if (this.name) this.people.push(this.name);
+    this.name = '';
+  }
+
+  showPeople() {
+    this.show = !this.show;
+  }
+
+  process(): string[] {
+    this.shuffledPeople = this.shuffleArray(this.people);
+    return this.shuffledPeople;
+  }
+
+
+  ////////////////////////////
+  /// Supporting functions ///
+  ////////////////////////////
+
+  shuffleArray(array: string[]): string[] {
+    if (!array || !array.length) return [];
+    let newArray = [...array].sort((a, b) => 0.5 - Math.random());;
+    return newArray;
+  }
 }
